0f2558bfdd8cd54be060d36dce0dbf96d00fbbf6
Dustin Schimel <dustin.schimel@gmail.com>
2017-09-07 13:54:25 -0600

    Messed with math and other C++ prior knowledge in Java

--- src/alone/controller/AloneController.java
+++ src/alone/controller/AloneController.java
@@ -7,5 +7,32 @@ public class AloneController
 	{
 		System.out.println("Ayyyy buddy");
 		System.out.println(";)");
+		
+		/************************************/
+		
+		System.out.println(4 + 4);
+		
+		int num1 = 77;
+		System.out.println(num1 + 3);
+		
+		String Word = "I like hotdogs";
+		System.out.println(Word + " and rice.");
+		
+		if (Word == "I like hotdogs")
+			System.out.println("Yesss");
+		else
+			System.out.println("Nooooo");
+		
+		boolean boolVar = true;
+		
+		if (boolVar == true)
+			System.out.println("This statement is true");
+		else
+			System.out.println("this statement is false");
+		
+		for(int i = 1; i < 6; i++)
+		{
+			System.out.println("Number " + i);
+		}
 	}
 }
