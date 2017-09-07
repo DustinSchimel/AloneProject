28193de11e8596736262103b5298741b8e5f8dc4
Dustin Schimel <dustin.schimel@gmail.com>
2017-09-07 12:49:59 -0600

    Initial commit, added gitignore, added controller and runner

---
+++ src/alone/controller/AloneController.java
@@ -0,0 +1,11 @@
+package alone.controller;
+
+public class AloneController
+{
+
+	public void start()
+	{
+		System.out.println("Ayyyy buddy");
+		System.out.println(";)");
+	}
+}
---
+++ src/alone/controller/AloneRunner.java
@@ -0,0 +1,10 @@
+package alone.controller;
+
+public class AloneRunner
+{
+	public static void main (String [ ] args)
+	{
+		AloneController appController = new AloneController();
+		appController.start();
+	}
+}
